0e587338a4e42f52a26c493f3e6c1753046c65c8
Amanda Leal <lealamanda1998@gmail.com>
2026-01-05 21:58:30 -0300

    Validating calendars with assertion

--- tests/Calendar.spec.js
+++ tests/Calendar.spec.js
@@ -1,15 +1,27 @@
-const {test, expect} = require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test.only('Calendar validations', async ({page}) => {
+test.only('Calendar validations', async ({ page }) => {
 
+    const calendar = page.locator(".react-calendar__navigation__label");
     const monthNumber = "6";
     const date = "15";
     const year = "2027";
+    const expectedList = [monthNumber, date, year];
 
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
-    await page.locator(".react-date-picker__inputGroup").click();
-    await page.locator(".react-calendar__navigation__label").click();
-    await page.locator(".react-calendar__navigation__label").click();
+    await page.locator(".react-date-picker__inputGroup").click(); // opens the calendar
+    await calendar.click(); // clicks on months
+    await calendar.click(); // clicks on years   
+    await page.getByRole('button', { name: year }).click(); // selects year
+    await page.locator(".react-calendar__year-view__months__month").nth(Number(monthNumber)-1).click(); // selects month and gives Number -1 because index starts from 0
+    await page.locator("//abbr[text()='" + date + "']").click(); // selects date
 
+    // assertion to verify if the date selected is correct
+    const inputs = page.locator('.react-date-picker__inputGroup__input'); // no need for await because this is not an action
+
+    for (let i=0; i<expectedList.length; i++) {  
+        const value = await inputs.nth(i).inputValue();
+        expect(value).toEqual(expectedList[i]);
+    }
     
 })
